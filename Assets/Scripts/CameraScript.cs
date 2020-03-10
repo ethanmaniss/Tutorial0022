6afbce415902007158fc586710b9714188fa10fd
ethanmaniss <54688877+ethanmaniss@users.noreply.github.com>
2020-03-09 23:20:02 -0700

    Initial commit

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("i am a camera"); // equivalent of Console.WriteLine()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
